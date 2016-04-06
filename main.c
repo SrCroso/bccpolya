e9b95ca1dd37e40dbf0534c2ecf9a5fffb0bcdb9
SrCroso <diogoronunes@gmail.com>
2016-04-05 22:07:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
+double raizq(double);
 
+int main(void) 
+{
+	double entrada;
+	scanf("%lf", &entrada);
+	
+	entrada = raizq(entrada);
+	
+	printf("%.2f\n", entrada);
+}
+
+double raizq(double entrada)
+{
+	double x1, x2 = 0.0;
+	
+	x1 = (entrada + 1.0)/2.0;
+	
+	while(x2 != x1)
+	{
+		x2 = (x1 + (entrada/x1))/2.0;
+		x1 = (x2 + (entrada/x2))/2.0;
+	}
+	
+	return x1;
+}
